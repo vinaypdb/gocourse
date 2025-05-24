527d5c3d691031c4a1f3e58f728eb540ac3485c4
vinaypdb <vinaypedapuri@gmail.com>
2025-05-24 16:49:46 +0530

    updated files

---
+++ 02_Variables/01_Shorthand/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := "golang"
+	c := 4.17
+	d := true
+	e := "Hello"
+	f := `Do you like my hat?`
+	g := 'M'
+
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+	fmt.Printf("%v \n", e)
+	fmt.Printf("%v \n", f)
+	fmt.Printf("%v \n", g)
+
+	fmt.Println()
+
+	fmt.Printf("%T \n", a)
+	fmt.Printf("%T \n", b)
+	fmt.Printf("%T \n", c)
+	fmt.Printf("%T \n", d)
+	fmt.Printf("%T \n", e)
+	fmt.Printf("%T \n", f)
+	fmt.Printf("%T \n", g)
+}
---
+++ 02_Variables/03_Multiple_Variables/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+    x, y := "Go", 2025
+    fmt.Println("x:", x)
+    fmt.Println("y:", y)
+}
+
